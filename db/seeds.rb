5331ed81c0dd98c8f5983de7dd5a4ff08901885d
Chris Smith <wingedkraby@gmail.com>
2016-10-23 15:00:04 +0000

    updated format

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,10 +6,31 @@
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
 
-powers = [{:name => "Summon Toddler", :description =>"Summon a small toddler to assist you"},
-          {:name => "Talk To Demons", :description =>"You can talk to demons for advice"},
-          {:name => "laser eyes", :description =>"you have laser vision, shit hurts tho"},
-          {:name => "Hatception", :description =>"You can make people think you are wearing a hat"}
+powers = [{:name => "Television", :description =>"You can distract someone telepathically for 2-3 seconds"},
+          {:name => "Lighting Fingers", :description =>"Through raising your hands you can produce an electric bolt from your fingers.  As a 
+ranged attack on your turn you may use this power.  However, the electricity you 
+produce is incredibly inaccurate.  When you make an attack of this nature you must roll a d6 if the result is a 2 or less you hit a random target within your vision.  Otherwise you hit your target with full damage. 
+"},
+          {:name => "Artic Feet", :description =>"Your feet are extremely cold!"},
+          {:name => "Animal Creeper", :description =>"The inner machinations of the minds of beasts are well known to you.  However while 
+	 you can know what an animal is thinking, you cannot influence the animal using your
+ 	 psychic powers."},
+          {:name => "Fire Hazzard", :description =>"Ability to make anything flammable"},
+          {:name => "FlameOff", :description =>"You are fire retardant!"},
+          {:name => "Inking", :description =>"Ability to make ink pens burst open"},
+          {:name => "Wall Phaze", :description =>" Ability to run through SOME walls"},
+          {:name => "LactoVision", :description =>"Ability to make someone temporarily lactose intolerant"},
+          {:name => "All Knowing Forgetfull", :description =>"Omniscient with Alzheimer's"},
+          {:name => "Hatception", :description =>"Everybody you meet thinks you are wearing a hat"},
+          {:name => "ToddlerMancy", :description =>"You have the ability to summon a toddler"},
+          {:name => "Democation", :description =>"you can talk to demons"},
+          {:name => "", :description =>""},
+          {:name => "", :description =>""},
+          {:name => "", :description =>""},
+          {:name => "", :description =>""},
+          {:name => "", :description =>""},
+          
+          
           ]
           
 powers.each do |power|
